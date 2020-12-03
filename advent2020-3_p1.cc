38845c8bab847e5543083c73b92fa6f242c0d672
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-12-03 09:30:13 +0100

    Add files via upload

---
+++ advent2020-3_p1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string paraula;
+    int count = 0, pos = 0;
+    while (cin >> paraula) {
+        if (pos > 30) pos = pos - 31;
+        if (paraula[pos] == '#') ++count;
+        pos += 3;
+    }
+    cout << count << endl;
+}
