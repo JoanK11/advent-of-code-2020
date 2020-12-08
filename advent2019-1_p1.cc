9b3ad99837de0420c76598b15c1db22bee05fe8a
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-12-08 13:25:43 +0100

    Add files via upload

---
+++ advent2019-1_p1.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total = 0, n;
+    while (cin >> n) total += (n/3) - 2;
+    cout << total << endl;
+}
+            
+            
